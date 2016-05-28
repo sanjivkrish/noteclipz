64fce67a6ec938c3cc056eb15278c36e3b3582da
sanjivkrish <sanjiv436.k@gmail.com>
2016-05-28 19:11:25 +0530

    js: mainCtrl: Support for adding new clip
    Popup template to user to get tag and content.
    Populate it to the view for the user
    
    Signed-off-by: sanjivkrish <sanjiv436.k@gmail.com>

--- www/js/mainCtrl.controller.js
+++ www/js/mainCtrl.controller.js
@@ -1,4 +1,53 @@
 angular.module('starter')
-	.controller('mainCtrl', function() {
+	.controller('mainCtrl', function($scope, $ionicPopup) {
 
+		//
+		// Array to store all clipz
+		//
+		$scope.allNoteClipz = [];
+
+		//
+		// Prompt user to enter input
+		//
+		$scope.addToClipz = function() {
+			$scope.data = {};
+
+			// An elaborate, custom popup
+			var myPopup = $ionicPopup.show({
+				template: '<input type="text" ng-model="data.tag" placeholder=" Tag"><br>' +
+				'<textarea ng-model="data.content" rows="4" placeholder=" Content"></textarea>',
+				title: 'Enter Tag and content',
+				scope: $scope,
+				buttons: [
+					{ text: 'Cancel' },
+					{
+						text: '<b>Save</b>',
+						type: 'button-positive',
+						onTap: function(e) {
+							if (!$scope.data.tag && !$scope.data.content) {
+								//don't allow the user to close unless he enters clip
+								e.preventDefault();
+							} else {
+								return $scope.data;
+							}
+						}
+					}
+				]
+			});
+
+			//
+			// Add to clipz collection
+			//
+			myPopup.then(function(res) {
+				if (res != undefined) {
+				    var obj = {};
+
+				    obj.tag = res.tag;
+					obj.content = res.content;
+					obj.date = '';
+
+					$scope.allNoteClipz.push(obj);
+				}
+			});
+		};
 	});
